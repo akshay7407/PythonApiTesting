910d031e4860b9fe60e120dafe7e425e4ac61d4e
akshaygaikwad6 <akshaygaikwad6@byjus.com>
2023-09-06 15:38:59 +0530

    new code

--- tests/restApiRequests/getRequest_withParam.py
+++ tests/restApiRequests/getRequest_withParam.py
@@ -1,5 +1,7 @@
 import requests
 
-param = {"name":"akshay","email":"akshayTesingWorl@gmail.com"}
-response = requests.get("https://httpbin.org/get",params=param)
-print(response.text)
+
+def test_get_request_with_parmm():
+    param = {"name":"akshay","email":"akshayTesingWorl@gmail.com"}
+    response = requests.get("https://httpbin.org/get",params=param)
+    print(response.text)
